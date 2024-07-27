a85a5675f8b318562c24c6394192b1767fd3ac8a
Wirelessscript <nieuwenhuisjohan309@gmail.com>
2024-07-26 20:15:11 -0400

    adding ShoppingCart

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import './ProductList.css'; 
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addItemToCart } from './CartSlice';
+import './ProductList.css'; // Import CSS file for component-specific styles
+
 
 const ProductList = () => {
-
+const dispatch = useDispatch();
+const [disableProducts, setDisabledProducts] = useState([]); //State to store disabled products
   const products = [
     { id: 1, name: 'Product A', price: 60 },
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
-
+const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+    setDisabledProducts([...setDisabledProducts, product.id]); //Mark the product as disabled
+};
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-     
+        {products.map(product => (
+            <li key={product.id} className="product-list-item">
+                <span>{product.name} - ${product.price}</span>
+                <button
+                    className={`add-to-cart-btn ${disableProducts.includes(product.id) ? 'disabled' : ''}`}
+                    onClick={()=> handleAddToCart(products)}
+                    disabled={disableProducts.includes(product.id)} //Disable button if product is in disabledProducts
+                > 
+                    Add to Chart
+             </button>
+            </li>
+         ))}
       </ul>
     </div>
   );
--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice';
 import './ShoppingCart.css'; 
 
 const ShoppingCart = () => {
-
+const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.cartItems);
+    const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    
   return (
     <>
     <div className="shopping-cart">
