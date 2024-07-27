f6416dd5d91ed74622d0ffbcf5bf9a235148ac04
Wirelessscript <nieuwenhuisjohan309@gmail.com>
2024-07-26 21:37:44 -0400

    Ecommerse

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -32,8 +32,8 @@ const ShoppingCart = () => {
         {cartItems.map(item => (
             <li key={item.id} className="cart-item">
                 <span>{item.name} - ${item.price}</span>
-                <div className="quantity=controls">
-                <button classname="quantity-control-btn" onClick={() => handleDecreaseQuantity(item.id)}>-</button>
+                <div className="quantity-controls">
+                <button className="quantity-control-btn" onClick={() => handleDecreaseQuantity(item.id)}>-</button>
                 <span> {item.quantity}</span>
                 <button className="quantity-control-btn" onClick={() => handleIncreaseQuantity(item.id)}>+</button>
                 </div>
@@ -43,7 +43,7 @@ const ShoppingCart = () => {
       </ul>
       <button className="clear-cart-btn" onClick={handleClearCart}>Clear Cart</button>
     </div>
-    <div>{totalAmount? <div>'The total amount is {totalAmount}</div> : ''}</div>
+    <div>{totalAmount ? <div>'The total amount is {totalAmount}</div> : ''}</div>
    </>
   );
 };
